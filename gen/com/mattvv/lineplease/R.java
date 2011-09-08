960e3b3daafa28caa814ed2292c0070c3c984e3b
Matt Van Veenendaal <m@mattvv.com>
2011-09-08 14:22:56 -0700

    add script

--- gen/com/mattvv/lineplease/R.java
+++ gen/com/mattvv/lineplease/R.java
@@ -14,28 +14,29 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int add=0x7f060004;
-        public static final int character=0x7f06000a;
-        public static final int createNewAccount=0x7f060011;
-        public static final int create_account=0x7f060009;
-        public static final int editText1=0x7f060014;
-        public static final int email=0x7f060007;
-        public static final int line=0x7f06000b;
+        public static final int add=0x7f060003;
+        public static final int character=0x7f06000b;
+        public static final int createNewAccount=0x7f060012;
+        public static final int create_account=0x7f06000a;
+        public static final int editText1=0x7f060015;
+        public static final int email=0x7f060008;
+        public static final int line=0x7f06000c;
         public static final int linearLayout1=0x7f060000;
-        public static final int listview=0x7f060015;
-        public static final int login=0x7f060010;
-        public static final int logout=0x7f060013;
-        public static final int messageLL=0x7f06000f;
+        public static final int listview=0x7f060016;
+        public static final int login=0x7f060011;
+        public static final int logout=0x7f060014;
+        public static final int messageLL=0x7f060010;
         public static final int name=0x7f060002;
-        public static final int password=0x7f060006;
-        public static final int play=0x7f06000d;
-        public static final int register=0x7f060008;
-        public static final int scroller=0x7f06000e;
-        public static final int send=0x7f06000c;
-        public static final int skip=0x7f060003;
+        public static final int password=0x7f060007;
+        public static final int play=0x7f06000e;
+        public static final int register=0x7f060009;
+        public static final int scroller=0x7f06000f;
+        public static final int send=0x7f06000d;
+        public static final int skip=0x7f060005;
         public static final int textView1=0x7f060001;
-        public static final int titleName=0x7f060012;
-        public static final int username=0x7f060005;
+        public static final int textView2=0x7f060004;
+        public static final int titleName=0x7f060013;
+        public static final int username=0x7f060006;
     }
     public static final class layout {
         public static final int addscript=0x7f030000;
@@ -47,8 +48,10 @@ public final class R {
         public static final int scripts=0x7f030006;
     }
     public static final class string {
+        public static final int add_button=0x7f050003;
         public static final int app_name=0x7f050001;
         public static final int hello=0x7f050000;
+        public static final int or=0x7f050002;
     }
     public static final class style {
         public static final int CustomTheme=0x7f040001;
