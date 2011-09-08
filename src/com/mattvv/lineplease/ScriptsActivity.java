36423ec8f27ef7c8c455e4ecbca822b9f8591c28
Matt Van Veenendaal <m@mattvv.com>
2011-09-08 13:33:10 -0700

    Deleting of Scripts

--- src/com/mattvv/lineplease/ScriptsActivity.java
+++ src/com/mattvv/lineplease/ScriptsActivity.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,7 +14,9 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.parse.DeleteCallback;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -37,10 +41,10 @@ public class ScriptsActivity extends Activity {
 		ctx = this;
 		listView = (ListView) findViewById(R.id.listview);
 
-		getScripts();
+		refreshScripts();
 	}
 
-	public void getScripts() {
+	public void refreshScripts() {
 		scriptNames = new ArrayList<String>();
 		scriptIds = new ArrayList<String>();
 
@@ -71,6 +75,63 @@ public class ScriptsActivity extends Activity {
 	public void setListView() {
 		listView.setAdapter(new ArrayAdapter<String>(this, R.layout.listviewrow, scriptNames));
 		listView.setOnItemClickListener(itemclicklistener);
+		listView.setOnItemLongClickListener(longClickListener);
+	}
+	
+	public void deleteScript(int position) {
+		Log.d("Delete", "Deleting Script");
+		final String scriptId = scriptIds.get(position);
+		String scriptName = scriptNames.get(position);
+		
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage("Are you sure you want to delete " + scriptName)
+		       .setCancelable(false)
+		       .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+		                removeScriptRemote(scriptId);
+		                refreshScripts();
+		           }
+		       })
+		       .setNegativeButton("No", new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+		                //don't remove it :P
+		           }
+		       });
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
+	
+	public void removeScriptRemote(String scriptId) {
+		//delete the script
+		
+		//todo: show a loading indicator
+		ParseQuery query = new ParseQuery("Script");
+		try {
+		    ParseObject script = query.get(scriptId);
+		    script.deleteInBackground(new DeleteCallback() {
+		        public void done(ParseException e) {
+		        	//todo: close a loading indicator
+		        	refreshScripts();
+		        }
+		    });
+		} catch (ParseException e) {
+		    // e.getMessage() will have information on the error.
+		}
+		
+		query = new ParseQuery("Line");
+		query.whereEqualTo("scriptId", scriptId);
+		query.findInBackground(new FindCallback() {
+		    public void done(List<ParseObject> lineList, ParseException e) {
+		        if (e == null) {
+		        	for (int i = 0; i < lineList.size(); i++) {
+		        		lineList.get(i).deleteInBackground();
+		        	}
+		        } else {
+		        }
+		    }
+		});
+		
+		
 	}
 
 	AdapterView.OnItemClickListener itemclicklistener = new AdapterView.OnItemClickListener() {
@@ -82,6 +143,16 @@ public class ScriptsActivity extends Activity {
 			Intent intent = new Intent(view.getContext(), LinesActivity.class);
 			startActivityForResult(intent, 0);
 
+		}
+
+	};
+	
+	AdapterView.OnItemLongClickListener longClickListener = new AdapterView.OnItemLongClickListener() {
+		@Override
+		public boolean onItemLongClick(AdapterView<?> parent, final View view, int position, long id) {
+			Toast.makeText(ctx, "Pressed Delete", Toast.LENGTH_LONG);
+			deleteScript(position);
+			return true;
 		}
 
 	};
