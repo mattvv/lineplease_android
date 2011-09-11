1c349a53103d0097dae6bf2b6271a2da7123664c
Matt Van Veenendaal <m@mattvv.com>
2011-09-10 18:38:25 -0700

    removed title bar

--- gen/com/mattvv/lineplease/R.java
+++ gen/com/mattvv/lineplease/R.java
@@ -13,9 +13,12 @@ public final class R {
     public static final class drawable {
         public static final int background=0x7f020000;
         public static final int bg=0x7f020001;
-        public static final int icon=0x7f020002;
-        public static final int paper=0x7f020003;
-        public static final int papertile=0x7f020004;
+        public static final int coursebg=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int paper=0x7f020004;
+        public static final int papertile=0x7f020005;
+        public static final int topbar=0x7f020006;
+        public static final int topbg=0x7f020007;
     }
     public static final class id {
         public static final int add=0x7f060003;
@@ -57,7 +60,11 @@ public final class R {
         public static final int or=0x7f050002;
     }
     public static final class style {
-        public static final int CustomTheme=0x7f040001;
-        public static final int CustomWindowTitleBackground=0x7f040000;
+        /** <style name="CustomWindowTitleBackground">
+        <item name="android:background">@drawable/topbg</item>
+    </style>
+ 
+         */
+        public static final int CustomTheme=0x7f040000;
     }
 }
