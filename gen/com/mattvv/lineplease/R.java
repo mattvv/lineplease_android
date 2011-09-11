c90dcd0fd1e7fd86750c4b4bd6c3d59883760b3a
Matt Van Veenendaal <m@mattvv.com>
2011-09-11 11:37:41 -0700

    purple inputs

--- gen/com/mattvv/lineplease/R.java
+++ gen/com/mattvv/lineplease/R.java
@@ -12,22 +12,25 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int bg=0x7f020001;
-        public static final int button_black=0x7f020002;
-        public static final int button_blue=0x7f020003;
-        public static final int button_purple=0x7f020004;
-        public static final int coursebg=0x7f020005;
-        public static final int empty=0x7f020006;
-        public static final int icon=0x7f020007;
-        public static final int logo=0x7f020008;
-        public static final int paper=0x7f020009;
-        public static final int papertile=0x7f02000a;
-        public static final int register=0x7f02000b;
-        public static final int tablebottom=0x7f02000c;
-        public static final int tablemid=0x7f02000d;
-        public static final int tabletop=0x7f02000e;
-        public static final int topbar=0x7f02000f;
-        public static final int topbg=0x7f020010;
+        public static final int barbg=0x7f020001;
+        public static final int bg=0x7f020002;
+        public static final int button_black=0x7f020003;
+        public static final int button_blue=0x7f020004;
+        public static final int button_purple=0x7f020005;
+        public static final int coursebg=0x7f020006;
+        public static final int empty=0x7f020007;
+        public static final int icon=0x7f020008;
+        public static final int input_purple=0x7f020009;
+        public static final int inputtop=0x7f02000a;
+        public static final int logo=0x7f02000b;
+        public static final int paper=0x7f02000c;
+        public static final int papertile=0x7f02000d;
+        public static final int register=0x7f02000e;
+        public static final int tablebottom=0x7f02000f;
+        public static final int tablemid=0x7f020010;
+        public static final int tabletop=0x7f020011;
+        public static final int topbar=0x7f020012;
+        public static final int topbg=0x7f020013;
     }
     public static final class id {
         public static final int add=0x7f060002;
@@ -38,7 +41,7 @@ public final class R {
         public static final int email=0x7f060007;
         public static final int line=0x7f06000b;
         public static final int linearLayout1=0x7f060000;
-        public static final int listview=0x7f060014;
+        public static final int listview=0x7f060016;
         public static final int listviewlines=0x7f06000e;
         public static final int login=0x7f06000f;
         public static final int logout=0x7f060012;
@@ -46,10 +49,12 @@ public final class R {
         public static final int password=0x7f060006;
         public static final int play=0x7f06000d;
         public static final int register=0x7f060008;
+        public static final int seach=0x7f060015;
         public static final int send=0x7f06000c;
         public static final int skip=0x7f060004;
         public static final int textView2=0x7f060003;
         public static final int titleName=0x7f060011;
+        public static final int topBar=0x7f060014;
         public static final int username=0x7f060005;
     }
     public static final class layout {
@@ -70,6 +75,7 @@ public final class R {
     public static final class style {
         public static final int ButtonText=0x7f040000;
         public static final int CustomTheme=0x7f040001;
+        public static final int TopBarInput=0x7f040003;
         public static final int tableInput=0x7f040002;
     }
 }
