14612377a7c109fa46a5157f2a642106fc7eb08c
Matt Van Veenendaal <m@mattvv.com>
2011-09-10 19:48:58 -0700

    more spacing, added logo

--- gen/com/mattvv/lineplease/R.java
+++ gen/com/mattvv/lineplease/R.java
@@ -14,14 +14,16 @@ public final class R {
         public static final int background=0x7f020000;
         public static final int bg=0x7f020001;
         public static final int coursebg=0x7f020002;
-        public static final int icon=0x7f020003;
-        public static final int paper=0x7f020004;
-        public static final int papertile=0x7f020005;
-        public static final int tablebottom=0x7f020006;
-        public static final int tablemid=0x7f020007;
-        public static final int tabletop=0x7f020008;
-        public static final int topbar=0x7f020009;
-        public static final int topbg=0x7f02000a;
+        public static final int empty=0x7f020003;
+        public static final int icon=0x7f020004;
+        public static final int logo=0x7f020005;
+        public static final int paper=0x7f020006;
+        public static final int papertile=0x7f020007;
+        public static final int tablebottom=0x7f020008;
+        public static final int tablemid=0x7f020009;
+        public static final int tabletop=0x7f02000a;
+        public static final int topbar=0x7f02000b;
+        public static final int topbg=0x7f02000c;
     }
     public static final class id {
         public static final int add=0x7f060003;
