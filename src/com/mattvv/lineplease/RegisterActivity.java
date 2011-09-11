f8e82d8a5d2b3ab4c923cbb632bffece2673c77d
Matt Van Veenendaal <m@mattvv.com>
2011-09-11 16:26:40 -0700

    auto login from register

--- src/com/mattvv/lineplease/RegisterActivity.java
+++ src/com/mattvv/lineplease/RegisterActivity.java
@@ -1,13 +1,18 @@
 package com.mattvv.lineplease;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 
@@ -38,6 +43,30 @@ public class RegisterActivity extends Activity {
 		user.setPassword(password.getText().toString());
 		user.setEmail(email.getText().toString());
 	}
+	
+	public void loginNow() {
+		EditText username = (EditText) findViewById(R.id.username);
+		EditText password = (EditText) findViewById(R.id.password);
+
+		ParseUser.logInInBackground(username.getText().toString(), password.getText().toString(), new LogInCallback() {
+			@Override
+			public void done(ParseUser user, ParseException e) {
+				if ((e == null) && (user != null)) {
+					loginSuccessful();
+				}
+			}
+		});
+	}
+	
+	public void loginSuccessful() {
+		InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+		EditText password = (EditText) findViewById(R.id.email);
+		imm.hideSoftInputFromWindow(password.getWindowToken(), 0);
+		
+		Toast.makeText(RegisterActivity.this, "Successful Login", Toast.LENGTH_LONG).show();
+		Intent intent = new Intent(this, ScriptsActivity.class);
+		startActivityForResult(intent, 0);
+	}
 
 	View.OnClickListener ButtonClickListeners = new View.OnClickListener() {
 
@@ -51,10 +80,11 @@ public class RegisterActivity extends Activity {
 					@Override
 					public void done(com.parse.ParseException e) {
 						// TODO Auto-generated method stub
-						if (e == null)
+						if (e == null) {
 							Toast.makeText(RegisterActivity.this, "Your Account has been Created", Toast.LENGTH_LONG).show();
-						else {
-							Log.e("Login failed", "Failed");
+							loginNow();
+						} else {
+							Log.e("Register failed", "Failed");
 							e.printStackTrace();
 							Toast.makeText(RegisterActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
 						}
